a350431b5102236c0022e7eaa50ca86d6aae71b8
Jeremy Hinegardner <jeremy@hinegardner.org>
2008-05-19 06:55:09 +0000

    update tests to match new sqlite3 version
    20080519065509-2f820-1cd8ea1161b4a26ea7fd6a714be3df1d37ca6f4b.gz

--- spec/sqlite3/version_spec.rb
+++ spec/sqlite3/version_spec.rb
@@ -5,10 +5,10 @@ describe "Amalgalite::SQLite3::Version" do
   it "should have the sqlite3 version" do
     Amalgalite::SQLite3::VERSION.should =~ /\d\.\d\.\d/
     Amalgalite::SQLite3::Version.to_s.should =~ /\d\.\d\.\d/
-    Amalgalite::SQLite3::Version.to_i.should == 3005008
+    Amalgalite::SQLite3::Version.to_i.should == 3005009
     Amalgalite::SQLite3::Version::MAJOR.should == 3
     Amalgalite::SQLite3::Version::MINOR.should == 5
-    Amalgalite::SQLite3::Version::RELEASE.should == 8
+    Amalgalite::SQLite3::Version::RELEASE.should == 9
     Amalgalite::SQLite3::Version.to_a.should have(3).items
   end
 end
