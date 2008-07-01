679e5d55909c727b4bf846f83e797048b1570cd1
Jeremy Hinegardner <jeremy@hinegardner.org>
2008-06-30 21:45:55 -0600

    Revision bump
      * update version number to 0.2.0
      * rename gem-db script
      * update HISTORY with explanations of examples

--- lib/amalgalite/version.rb
+++ lib/amalgalite/version.rb
@@ -8,8 +8,8 @@ module Amalgalite
   module Version
 
     MAJOR   = 0
-    MINOR   = 1
-    BUILD   = 1
+    MINOR   = 2
+    BUILD   = 0
 
     #
     # return the Version as an array of MAJOR, MINOR, BUILD
