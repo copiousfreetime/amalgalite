2f887ba1ad9b209563d2abf8d025fd0fadca40e5
Jeremy Hinegardner <jeremy@hinegardner.org>
2013-03-13 14:06:01 +0100

    update to sqlite 3.7.15.2

--- spec/sqlite3/version_spec.rb
+++ spec/sqlite3/version_spec.rb
@@ -7,16 +7,16 @@ describe "Amalgalite::SQLite3::Version" do
     Amalgalite::SQLite3::Version.to_s.should =~ /\d\.\d\.\d/
     Amalgalite::SQLite3::Version.runtime_version.should =~ /\d\.\d\.\d/
 
-    Amalgalite::SQLite3::Version.to_i.should eql(3007014)
-    Amalgalite::SQLite3::Version.runtime_version_number.should eql(3007014)
+    Amalgalite::SQLite3::Version.to_i.should eql(3007015)
+    Amalgalite::SQLite3::Version.runtime_version_number.should eql(3007015)
 
     Amalgalite::SQLite3::Version::MAJOR.should eql(3)
     Amalgalite::SQLite3::Version::MINOR.should eql(7)
-    Amalgalite::SQLite3::Version::RELEASE.should eql(14)
+    Amalgalite::SQLite3::Version::RELEASE.should eql(15)
     Amalgalite::SQLite3::Version.to_a.should have(3).items
 
-    Amalgalite::SQLite3::Version.compiled_version.should be == "3.7.14"
-    Amalgalite::SQLite3::Version.compiled_version_number.should be == 3007014
+    Amalgalite::SQLite3::Version.compiled_version.should be == "3.7.15.2"
+    Amalgalite::SQLite3::Version.compiled_version_number.should be == 3007015
     Amalgalite::SQLite3::Version.compiled_matches_runtime?.should be == true
   end
 end
