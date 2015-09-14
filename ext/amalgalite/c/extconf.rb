8496a044522f9c2e84893818a0ced90b6868c255
Jeremy Hinegardner <jeremy@copiousfreetime.org>
2015-09-13 23:17:04 -0600

    update to have different compiler warnings silence by compiler

--- ext/amalgalite/c/extconf.rb
+++ ext/amalgalite/c/extconf.rb
@@ -27,7 +27,14 @@ end
   RbConfig::MAKEFILE_CONFIG['debugflags'] = RbConfig::MAKEFILE_CONFIG['debugflags'].gsub(/#{debug}/,'')   if RbConfig::MAKEFILE_CONFIG['debugflags']
 end
 
-%w[ unused-const-variable shorten-64-to-32 write-strings incompatible-pointer-types-discards-qualifiers].each do |warning|
+ignoreable_warnings = %w[ write-strings ]
+ignore_by_compiler = { "clang" => %w[ unused-const-variable shorten-64-to-32 incompatible-pointer-types-discards-qualifiers] }
+
+if extras = ignore_by_compiler[RbConfig::MAKEFILE_CONFIG["CC"]] then
+  ignoreable_warnings.concat(extras)
+end
+
+ignoreable_warnings.each do |warning|
   $CFLAGS = $CFLAGS.gsub(/-W#{warning}/,'')
   RbConfig::MAKEFILE_CONFIG['warnflags'] = RbConfig::MAKEFILE_CONFIG['warnflags'].gsub(/-W#{warning}/,'') if RbConfig::MAKEFILE_CONFIG['warnflags'] 
   $CFLAGS += " -Wno-#{warning}"
