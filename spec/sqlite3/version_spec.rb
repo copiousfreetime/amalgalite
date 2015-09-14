f7e038623796c6400326c5b96c5b293a3576fb64
Jeremy Hinegardner <jeremy@copiousfreetime.org>
2015-09-13 19:59:11 -0600

    update sqlite3 version tests

--- spec/sqlite3/version_spec.rb
+++ spec/sqlite3/version_spec.rb
@@ -7,16 +7,16 @@ describe "Amalgalite::SQLite3::Version" do
     expect(Amalgalite::SQLite3::Version.to_s).to match( /\d\.\d\.\d/ )
     expect(Amalgalite::SQLite3::Version.runtime_version).to match( /\d\.\d\.\d/ )
 
-    Amalgalite::SQLite3::Version.to_i.should eql(3008010)
-    Amalgalite::SQLite3::Version.runtime_version_number.should eql(3008010)
+    Amalgalite::SQLite3::Version.to_i.should eql(3008011)
+    Amalgalite::SQLite3::Version.runtime_version_number.should eql(3008011)
 
     Amalgalite::SQLite3::Version::MAJOR.should eql(3)
     Amalgalite::SQLite3::Version::MINOR.should eql(8)
-    Amalgalite::SQLite3::Version::RELEASE.should eql(10)
+    Amalgalite::SQLite3::Version::RELEASE.should eql(11)
     expect(Amalgalite::SQLite3::Version.to_a.size).to eql(3)
 
-    Amalgalite::SQLite3::Version.compiled_version.should be == "3.8.10.2"
-    Amalgalite::SQLite3::Version.compiled_version_number.should be == 3008010
+    Amalgalite::SQLite3::Version.compiled_version.should be == "3.8.11.1"
+    Amalgalite::SQLite3::Version.compiled_version_number.should be == 3008011
     Amalgalite::SQLite3::Version.compiled_matches_runtime?.should be == true
   end
 end
