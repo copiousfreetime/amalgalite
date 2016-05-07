25e576d4b98d2bcb439cf5e9fa8d768ffa79fc27
Jeremy Hinegardner <jeremy@copiousfreetime.org>
2016-05-07 10:32:58 -0600

    deal with 2 digit vesion in sqlite

--- spec/sqlite3/version_spec.rb
+++ spec/sqlite3/version_spec.rb
@@ -3,20 +3,20 @@ require 'amalgalite/sqlite3/version'
 
 describe "Amalgalite::SQLite3::Version" do
   it "should have the sqlite3 version" do
-    expect(Amalgalite::SQLite3::VERSION).to match(/\d\.\d\.\d/)
-    expect(Amalgalite::SQLite3::Version.to_s).to match( /\d\.\d\.\d/ )
-    expect(Amalgalite::SQLite3::Version.runtime_version).to match( /\d\.\d\.\d/ )
+    expect(Amalgalite::SQLite3::VERSION).to match(/\d+\.\d+\.\d+/)
+    expect(Amalgalite::SQLite3::Version.to_s).to match( /\d+\.\d+\.\d+/ )
+    expect(Amalgalite::SQLite3::Version.runtime_version).to match( /\d+\.\d+\.\d+/ )
 
-    Amalgalite::SQLite3::Version.to_i.should eql(3009002)
-    Amalgalite::SQLite3::Version.runtime_version_number.should eql(3009002)
+    Amalgalite::SQLite3::Version.to_i.should eql(3012002)
+    Amalgalite::SQLite3::Version.runtime_version_number.should eql(3012002)
 
     Amalgalite::SQLite3::Version::MAJOR.should eql(3)
-    Amalgalite::SQLite3::Version::MINOR.should eql(9)
+    Amalgalite::SQLite3::Version::MINOR.should eql(12)
     Amalgalite::SQLite3::Version::RELEASE.should eql(2)
     expect(Amalgalite::SQLite3::Version.to_a.size).to eql(3)
 
-    Amalgalite::SQLite3::Version.compiled_version.should be == "3.9.2"
-    Amalgalite::SQLite3::Version.compiled_version_number.should be == 3009002
+    Amalgalite::SQLite3::Version.compiled_version.should be == "3.12.2"
+    Amalgalite::SQLite3::Version.compiled_version_number.should be == 3012002
     Amalgalite::SQLite3::Version.compiled_matches_runtime?.should be == true
   end
 end
