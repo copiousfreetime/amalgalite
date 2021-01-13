8095fb95a243ed74f1e52fad907a66530e0bc9d4
copiousfreetime <jeremy@copiousfreetime.org>
2021-01-12 17:12:42 -0700

    update version spec for new sqlite3 version

--- spec/sqlite3/version_spec.rb
+++ spec/sqlite3/version_spec.rb
@@ -7,21 +7,21 @@ describe "Amalgalite::SQLite3::Version" do
     expect(Amalgalite::SQLite3::Version.to_s).to match( /\d+\.\d+\.\d+/ )
     expect(Amalgalite::SQLite3::Version.runtime_version).to match( /\d+\.\d+\.\d+/ )
 
-    Amalgalite::SQLite3::Version.to_i.should eql(3026000)
-    Amalgalite::SQLite3::Version.runtime_version_number.should eql(3026000)
+    Amalgalite::SQLite3::Version.to_i.should eql(3034000)
+    Amalgalite::SQLite3::Version.runtime_version_number.should eql(3034000)
 
     Amalgalite::SQLite3::Version::MAJOR.should eql(3)
-    Amalgalite::SQLite3::Version::MINOR.should eql(26)
+    Amalgalite::SQLite3::Version::MINOR.should eql(34)
     Amalgalite::SQLite3::Version::RELEASE.should eql(0)
     expect(Amalgalite::SQLite3::Version.to_a.size).to eql(3)
 
-    Amalgalite::SQLite3::Version.compiled_version.should be == "3.26.0"
-    Amalgalite::SQLite3::Version.compiled_version_number.should be == 3026000
+    Amalgalite::SQLite3::Version.compiled_version.should be == "3.34.0"
+    Amalgalite::SQLite3::Version.compiled_version_number.should be == 3034000
     Amalgalite::SQLite3::Version.compiled_matches_runtime?.should be == true
   end
 
   it "should have the sqlite3 source id" do
-    source_id = "2018-12-01 12:34:55 bf8c1b2b7a5960c282e543b9c293686dccff272512d08865f4600fb58238b4f9"
+    source_id = "2020-12-01 16:14:00 a26b6597e3ae272231b96f9982c3bcc17ddec2f2b6eb4df06a224b91089fed5b"
     Amalgalite::SQLite3::Version.compiled_source_id.should be == source_id
     Amalgalite::SQLite3::Version.runtime_source_id.should be == source_id
   end
