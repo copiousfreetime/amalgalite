80da813f0e4c3f937137904d26d187652f810ede
copiousfreetime <jeremy@copiousfreetime.org>
2022-06-17 17:31:18 -0600

    explicit test failure for CI

--- spec/version_spec.rb
+++ spec/version_spec.rb
@@ -5,4 +5,8 @@ describe "Amalgalite::VERSION" do
   it "should have a version string" do
     expect(Amalgalite::VERSION).to match( /\d+\.\d+\.\d+/ )
   end
+
+  it "should fail as a test" do
+    expect(true).to match(false)
+  end
 end
