14c58539b266c70a193a9901c0d5bb70e87bddb7
copiousfreetime <jeremy@copiousfreetime.org>
2022-06-17 17:43:03 -0600

    remove failing test

--- spec/version_spec.rb
+++ spec/version_spec.rb
@@ -5,8 +5,4 @@ describe "Amalgalite::VERSION" do
   it "should have a version string" do
     expect(Amalgalite::VERSION).to match( /\d+\.\d+\.\d+/ )
   end
-
-  it "should fail as a test" do
-    expect(true).to match(false)
-  end
 end
