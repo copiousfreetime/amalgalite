96558c585aa6d0665b086a8749ab665733600d8c
Jeremy Hinegardner <jeremy@copiousfreetime.org>
2025-02-17 19:27:16 -0700

    remove the amalgalite requires and packer code

--- examples/blob.rb
+++ examples/blob.rb
@@ -20,7 +20,6 @@ require 'rubygems'
 $: << "../lib"
 $: << "../ext"
 require 'amalgalite'
-require 'amalgalite/packer'
 VALID_ACTIONS = %w[ list retrieve store ]
 def usage 
   STDERR.puts "Usage: #{File.basename($0)} ( #{VALID_ACTIONS.join(' | ')} )  file(s)"
@@ -32,13 +31,26 @@ end
 #
 usage if ARGV.size < 1
 action    = ARGV.shift
-usage unless VALID_ACTIONS.include? action 
+usage unless VALID_ACTIONS.include? action
 file_list = ARGV
 
 #
 # create the database if it doesn't exist
 #
 db = Amalgalite::Database.new( "filestore.db" )
+schema = db.schema
+unless schema.tables['rubylibs']
+  puts "Create schema"
+  db.execute <<~SQL
+  CREATE TABLE rubylibs (
+    id            INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    filename      TEXT UNIQUE,
+    is_compressed BOOLEAN,
+    contents      BLOB
+  );
+  SQL
+  db.reload_schema!
+end
 
 case action
   #
@@ -63,11 +75,24 @@ when 'list'
   #
 when 'store'
   usage if file_list.empty?
-  require 'amalgalite/packer'
+  blob_column = db.schema.tables['rubylibs'].columns['contents']
+  db.transaction do |trans|
+    file_list.each do |filename|
+      msg = "  -> #{filename}"
+      begin
+        trans.execute("DELETE FROM rubylibs where filename = ?", filename)
 
-  packer = Amalgalite::Packer.new( :dbfile => 'filestore.db',
-                                   :compressed => false )
-  packer.pack( file_list )
+        trans.prepare("INSERT INTO rubylibs(filename, is_compressed, contents) VALUES( $filename, $compressed, $contents)") do |stmt|
+          stmt.execute( "$filename" => filename,
+                        "$compressed" => false,
+                        "$contents" => Amalgalite::Blob.new(file: filename,
+                                                            column: blob_column))
+
+          STDERR.puts "#{msg} : stored"
+        end
+      end
+    end
+  end
 
   #
   # dump the file that matches the right path to stdout.  This also shows
@@ -75,11 +100,12 @@ when 'store'
   #
 when 'retrieve'
   usage if file_list.empty?
-  db.execute("SELECT id, compressed, filename, contents FROM rubylibs WHERE filename = ?", file_list.first) do |row|
+  db.execute("SELECT id, is_compressed, filename, contents FROM rubylibs WHERE filename = ?", file_list.first) do |row|
     STDERR.puts "Dumping #{row['filename']} to stdout"
-    if row['compressed'] then
+    if row['is_compressed'] then
       s = row['contents'].to_s
-      STDOUT.puts Amalgalite::Packer.gunzip( s )
+      data = StringIO.new( s )
+      STDOUT.puts Zlib::GzipReader.new(data).read
     else
       row['contents'].write_to_io( STDOUT )
     end
